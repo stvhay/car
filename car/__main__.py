5cfe31191821090b78c2bf446200ea0942ad2f1c
Steven Hay <hay.steve@gmail.com>
2020-02-08 12:30:28 -0500

    reorg. add .credentials capability

---
+++ car/__main__.py
@@ -0,0 +1,4 @@
+import car.get_info
+
+if __name__ == '__main__':
+    car.get_info.main()
--- car/get_info.py
+++ car/get_info.py
@@ -4,12 +4,28 @@ import getpass
 import yaml
 from car.car_rest import RESTSession
 
-if __name__ == '__main__':
+
+def main():
+    """
+    Main function reads Tesla car API and outputs Yaml.
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument("username")
+    parser.add_argument('-u', '--username', help='Specify username')
     args = parser.parse_args()
-    username = args.username
-    password = getpass.getpass()
+
+    try:
+        with open('.credentials') as f:
+            c = yaml.safe_load(f.read())
+            password = c.get('password')
+            if not password:
+                getpass.getpass("Password")
+            username = c.get('username')
+            if not username:
+                username = input("Username: ")
+            if args.username:
+                username = args.username
+    except IOError:
+        password = getpass.getpass()
 
     d = {}
     endpoints = ['vehicle_data', 'service_data', 'data_request/charge_state', 'data_request/climate_state',
@@ -28,3 +44,7 @@ if __name__ == '__main__':
             info = yaml.safe_load(r.text)
             entry[endpoint] = info
     print(yaml.dump(d))
+
+
+if __name__ == '__main__':
+    main()
