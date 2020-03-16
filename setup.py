a82bf6ede67ebb635c15227bda118c21bd818613
Steven Hay <hay.steve@gmail.com>
2020-03-16 19:23:38 -0400

    Updated requirements.

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ from distutils.core import setup
 setup(name='car',
       version='0.0.1',
       description='Fun with the Tesla API',
-      packages=['car']
-     )
+      packages=['car'],
+      install_requires=['pyyaml', 'requests'])
